49614b45aa7208020184c14a7347c7137a6e4237
Zachary Lam <zlam@rrc.ca>
2025-12-05 17:50:24 -0600

    Changed application name to Activity Recommender

---
+++ activity-recommender/activity_recommenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  activity_recommenderApp.swift
+//  activity-recommender
+//
+//  Created by Zachary Lâm on 2025-12-05.
+//
+
+import SwiftUI
+
+@main
+struct activity_recommenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
